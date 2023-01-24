07eddd4437172fa917dd7486de2632653a24e3ed
Paul Trifan <93189720+paulxcode@users.noreply.github.com>
2023-01-24 13:15:41 +0200

    Add files via upload

---
+++ scripts-site2.js
@@ -0,0 +1,23 @@
+function scrollNews() {
+  var news = document.getElementById("news-bar");
+  var newsItems = news.getElementsByTagName("li");
+  var width = 0;
+
+  // calculate the width of the news items
+  for (var i = 0; i < newsItems.length; i++) {
+    width += newsItems[i].offsetWidth;
+  }
+
+  // set the width of the ul element to be the width of the news items
+  news.getElementsByTagName("ul")[0].style.width = width + "px";
+
+  // animate the ul element to the left
+  var pos = 0;
+  setInterval(function() {
+    pos--;
+    news.getElementsByTagName("ul")[0].style.left = pos + "px";
+  }, 30);
+}
+
+scrollNews();
+  
